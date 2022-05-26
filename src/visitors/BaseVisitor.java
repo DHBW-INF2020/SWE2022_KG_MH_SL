9b75acb2ed7cee77b5bafc2838fe3994527ff490
Kevin_Laptop <jaspergavagan@gmail.com>
2022-05-26 17:53:45 +0200

    FIX: Not Object is being returned anymore but the generic Type T

--- src/visitors/BaseVisitor.java
+++ src/visitors/BaseVisitor.java
@@ -8,17 +8,17 @@ import tree.Transponder;
 public class BaseVisitor<T> implements Visitor{
 
 	@Override
-	public Object visitSatellite(Satellite s) {
+	public T visitSatellite(Satellite s) {
 		return visitChildren(s);
 	}
 
 	@Override
-	public Object visitTransponder(Transponder t) {
+	public T visitTransponder(Transponder t) {
 		return visitChildren(t);
 	}
 
 	@Override
-	public Object visitChannel(Channel c) {
+	public T visitChannel(Channel c) {
 		return visitChildren(c);
 	}
 	

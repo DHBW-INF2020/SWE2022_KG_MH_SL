987a6af2789ceedd91e8fe014b37ad1bad81eab2
Kevin_Laptop <jaspergavagan@gmail.com>
2022-05-28 11:47:22 +0200

    ADD: First Aggreagte implemented as a visitor

---
+++ src/aggregates/SatelliteTransportsAggregate.java
@@ -0,0 +1,36 @@
+package aggregates;
+
+import tree.Node;
+import tree.Root;
+import tree.Satellite;
+import tree.Transponder;
+import visitors.BaseVisitor;
+
+public class SatelliteTransportsAggregate extends BaseVisitor<Node>{
+	
+	@Override
+	public Node visitRoot(Root r) {
+		System.out.println("Root");
+		Root root = new Root();
+		for(Node n : r.children()) {
+			root.addNode((Node) n.accept(this));
+		}
+		return root;
+	}
+	
+	@Override
+	public Node visitSatellite(Satellite s) {
+		System.out.println("Satellite");
+		Satellite satellite = new Satellite(s);
+		for(Node n : s.children()) {
+			satellite.addNode((Node) n.accept(this));
+		}
+		return satellite;
+	}
+
+	@Override
+	public Node visitTransponder(Transponder t) {
+		System.out.println("Transponder");
+		return new Transponder(t);
+	}
+}

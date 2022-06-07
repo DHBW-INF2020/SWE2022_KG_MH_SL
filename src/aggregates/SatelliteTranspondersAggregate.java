77ec2c51c48c9d955185dd56075490c16d3604ae
Kgavaga <jaspergavagan@gmail.com>
2022-06-07 20:58:05 +0200

    ADD: comments; REMOVE: logging syscalls

--- src/aggregates/SatelliteTranspondersAggregate.java
+++ src/aggregates/SatelliteTranspondersAggregate.java
@@ -7,13 +7,13 @@ import tree.Transponder;
 import visitors.BaseVisitor;
 
 /**
- *
+ *	Aggregates Transponders under the individual Satellites by
+ *	traversing through the tree based on a visitor pattern
  */
 public class SatelliteTranspondersAggregate extends BaseVisitor<Node>{
 	
 	@Override
 	public Node visitRoot(Root r) {
-		System.out.println("Root");
 		Root root = new Root();
 		for(Node n : r.children()) {
 			root.addNode((Node) n.accept(this));
@@ -23,7 +23,6 @@ public class SatelliteTranspondersAggregate extends BaseVisitor<Node>{
 	
 	@Override
 	public Node visitSatellite(Satellite s) {
-		System.out.println("Satellite");
 		Satellite satellite = new Satellite(s);
 		for(Node n : s.children()) {
 			satellite.addNode((Node) n.accept(this));
@@ -33,7 +32,6 @@ public class SatelliteTranspondersAggregate extends BaseVisitor<Node>{
 
 	@Override
 	public Node visitTransponder(Transponder t) {
-		System.out.println("Transponder");
 		return new Transponder(t);
 	}
 }

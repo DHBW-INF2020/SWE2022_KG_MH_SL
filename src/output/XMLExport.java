570a0522e958ceb55a1612b4a93109fbcac1c741
Your Name <you@example.com>
2022-06-12 11:56:34 +0200

    EDIT: create xml-string

--- src/output/XMLExport.java
+++ src/output/XMLExport.java
@@ -4,17 +4,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedWriter;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.XML;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import java.io.Writer;
 
 public class XMLExport implements IExport{
 
     public void export(StringBuilder jsonTree) throws JSONException{
         try {
+            
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            StringBuilder builder = new StringBuilder();
             FileWriter fileWriter = new FileWriter("./output/output.xml");
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            String printString = jsonTree.toString();
-            String xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>\n<root>" + XML.toString(printString) + "</root>";
+            String singleString = jsonTree.toString();
+            JsonParser jsonParser = new JsonParser();
+            JSONArray json = new JSONArray(gson.toJson(jsonParser.parse(singleString)));
+            String xml = XML.toString(json, "root");
+
             bufferedWriter.write(xml);
             bufferedWriter.flush();
             bufferedWriter.close();
@@ -24,5 +64,10 @@ public class XMLExport implements IExport{
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        catch (Exception e) {
+        throw new RuntimeException("Error occurs when pretty-printing xml:\n", e);
+        }
+
     }
 }
+

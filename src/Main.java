074cba64d2f20da6ab903c6cdd53d0777f046c13
NivisPluma <matzeminecraft@gmail.com>
2022-06-12 13:45:18 +0200

    ADD: Dokumentation for MAIN

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,14 @@ public class Main {
 
     /**
      * @param Args
+     *
+     * Variant 1: no Args
+     *
+     * Variant 2: 1 Arg -> Path of Config File (JSON)
+     * "D:\Dateien Schnell\Java\SWE2022_KG_MH_SL\input\program_configuration_2.json"
+     *
+     * Variant 3: 4 Args (AggregateType OutputFileType sourceJSONFile outputPath)
+     * sta json "D:\Dateien Schnell\Java\SWE2022_KG_MH_SL\res\Aufgabe_3_satellites.json" "D:\Dateien Schnell\Java"
      */
     public static void main(String[] Args) throws Exception {
         InputHandler inputHandler = new InputHandler();

c0b761efcd02c3aaea1548aab69798f5fd65c9c0
NivisPluma <matzeminecraft@gmail.com>
2022-06-12 13:38:08 +0200

    ADD: Program now runs according to Config (runProgramWithConfig()) EDIT: moved Program routine from Main to ProgramConfiguration

--- src/Main.java
+++ src/Main.java
@@ -32,26 +32,13 @@ public class Main {
     /**
      * @param Args
      */
-    public static void main(String[] Args) throws JSONException {
+    public static void main(String[] Args) throws Exception {
         InputHandler inputHandler = new InputHandler();
 
         // create new Configuration for Program
         ProgramConfiguration currentConfiguration = inputHandler.readInput(Args);;
-
-        System.out.println(currentConfiguration.getAggregateType());
-
-        JsonToTreeConverter inputConverter = new JsonToTreeConverter();
-        ChannelSatellitesAggregate csa = new ChannelSatellitesAggregate();
-        Root returnTree = (Root) inputConverter.readInputJson("./res/Aufgabe_3_satellites.json").accept(csa);
-
-        TreeToJsonConverter outputConverter = new TreeToJsonConverter();
-        StringBuilder jsonStringBuilder = (StringBuilder) returnTree.accept(outputConverter);
-//            StringBuilder jsonStringBuilder = (StringBuilder) root.accept(outputConverter);
-
-        JSONExport export_json = new JSONExport();
-        export_json.export(jsonStringBuilder);
-        XMLExport export_xml = new XMLExport();
-        export_xml.export(jsonStringBuilder);
+        // run Program
+        currentConfiguration.runProgramWithConfig();
     }
 }
 
--- src/input/InputHandler.java
+++ src/input/InputHandler.java
@@ -9,20 +9,27 @@ import java.io.Reader;
 public class InputHandler {
     public InputHandler(){};
 
-    public ProgramConfiguration readInput(String[] Args)
-    {
+    public ProgramConfiguration readInput(String[] Args) throws Exception {
         ProgramConfiguration currentConfiguration = null;
+
+        // default config
         if(Args.length == 0)
         {
-            currentConfiguration = createProgramConfiguration("./input/program_configuration.json");
+            currentConfiguration = createProgramConfiguration("./input/default_configuration.json");
+            System.out.println("no flags found - using default config ./input/default_configuration.json");
         }
 
+        // config file
         else if (Args.length == 1) {
+            System.out.println("debug - else if readInput");
+
             currentConfiguration = createProgramConfiguration(Args[0]);
         }
 
+        // config with flags
         else
         {
+            System.out.println("debug - else read input");
             currentConfiguration = new ProgramConfiguration(Args);
         }
         return currentConfiguration;
